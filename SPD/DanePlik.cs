3e3d276e76c834399e078f31dd818e67247ebfa9
lukasmas <mastalerzl12@gmail.com>
2019-03-08 22:43:14 +0100

    first commit

---
+++ SPD/DanePlik.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SPD
+{
+    class DanePlik
+    {
+        string nazwa;
+        int x { get; set; }
+        int y { get; set; }
+        int[] array;
+
+        public DanePlik(string nazwa_plik, int h, int w)
+        {
+            nazwa = nazwa_plik;
+            x = w;
+            y = h;
+            array = new int[x * y];
+        }
+
+        
+
+
+
+        
+    }
+}
--- SPD/MainWindow.xaml.cs
+++ SPD/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+
 
 namespace SPD
 {
@@ -20,9 +23,60 @@ namespace SPD
     /// </summary>
     public partial class MainWindow : Window
     {
+        FileStream dane;
+        DanePlik[] danePliks = new DanePlik[120];
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
+
+        }
+
+        public void LoadData()
+        {
+
+
+            OpenFileDialog openDialog = new OpenFileDialog();
+
+            if (openDialog.ShowDialog() == true)
+            {
+         
+                dane = new FileStream(openDialog.FileName, FileMode.Open, FileAccess.Read);
+            }
+
+        }
+
+        public void UseData()
+        {
+            StreamReader sr = new StreamReader(dane);
+            int i = 0;
+            
+
+            while (!sr.EndOfStream)
+            {
+                
+                if(sr.ReadLine().Contains("ta"))
+                {
+                    string numer = i.ToString;
+                    string nazwa = "ta" + numer;
+                    //int w, h;
+
+
+
+                    //danePliks[i] = new DanePlik();
+                }
+                
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+            UseData();
         }
     }
+
+        
 }
