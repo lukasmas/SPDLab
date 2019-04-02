6eed9dd3602778cce95b985f8f8897884056df8e
PawelTasiemiec <48258145+PawelTasiemiec@users.noreply.github.com>
2019-04-02 14:19:28 +0200

    Add files via upload
    Added appealing, working on neh button, not done yet, need a fix

---
+++ appealing/SPD/Zadanie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPD
+{
+    class Zadanie
+    {
+        public Zadanie(int id, int[] arr, int timeAll=0)
+        {
+            this.id = id;
+            this.arr = arr;
+            this.timeAll = timeAll;
+
+        }
+        public int id { get; set; }
+        public int [] arr { get; set; }
+        public int timeAll { get; set; }
+    }
+}
